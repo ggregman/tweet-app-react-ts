943bf232451e5ba124133859cff10fe438c990d8
ggregman <gregorio.manahan@gmail.com>
2023-01-09 16:52:40 +0100

    aggiunto type a useState

--- src/hooks/useTweets.ts
+++ src/hooks/useTweets.ts
@@ -1,8 +1,9 @@
 import { useCallback, useState } from "react";
 import { getTweetList } from "../api/getTweetList";
+import { TweetType } from "../types/tweetType";
 
 export const useTweets = () => {
-  const [tweetList, setTweetList] = useState(useCallback(getTweetList, []));
+  const [tweetList, setTweetList] = useState<TweetType[]>(useCallback(getTweetList, []));
 
   return { tweetList, setTweetList };
 };
